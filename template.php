7b3596e3a1bf3d60ae36c87dcd49ea817b85e458
Jurriaan Roelofs <info@jurr.me>
2017-02-22 16:05:03 +0100

    moved some code from template.php to glazed_preprocess_html.inc to solve wyss customer bugs. also fixes page_top page_bottom seven theme warnings

--- template.php
+++ template.php
@@ -4,21 +4,6 @@
  * @file template.php
  */
 
-// Load Glazed Theme Settings CSS File
-global $theme;
-$files_path = variable_get('file_public_path', conf_path() . '/files');
-if (is_file($files_path . '/glazed-themesettings-' . $theme . '.css')) {
-  drupal_add_css(
-    $files_path . '/glazed-themesettings-' . $theme . '.css', array(
-      'preprocess' => variable_get('preprocess_css', '') == 1 ? TRUE : FALSE,
-      'group' => CSS_THEME,
-      'media' => 'all',
-      'every_page' => TRUE,
-      'weight' => 100
-    )
-  );
-}
-
 /**
  * Load template.php logic from theme features
  */
