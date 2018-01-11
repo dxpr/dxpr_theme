f4945be02d21e99334e1ce89321564860c45ac86
Jurriaan Roelofs <info@jurr.me>
2018-01-11 11:46:06 +0100

    moved glazedNavFastInit to template.php so it can be called on earlier

--- template.php
+++ template.php
@@ -18,3 +18,27 @@ foreach (file_scan_directory(drupal_get_path('theme', 'glazed') . '/features', '
 foreach (file_scan_directory(drupal_get_path('theme', 'glazed') . '/includes', '/.inc/i') as $file) {
   require_once($file->uri);
 }
+
+function glazedNavFastInit() {
+  if ($breakpoint = theme_get_setting('header_mobile_breakpoint')) {
+    if ($breakpoint > 4099) {
+      $breakpoint = 99999;
+    }
+  }
+  else {
+    $breakpoint = 1200;
+  }
+  $nav_init = <<<EOT
+  <script>
+  var glazedNavBreakpoint = {$breakpoint};
+  var glazedWindowWidth = window.innerWidth;
+  if (glazedWindowWidth > glazedNavBreakpoint) {
+    document.body.className += ' body--glazed-nav-desktop';
+  }
+  else {
+    document.body.className += ' body--glazed-nav-mobile';
+  }
+  </script>
+EOT;
+  return $nav_init;
+}
