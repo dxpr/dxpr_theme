b6ac0419b9e36f48f4ce372d81fc9f04ab744d2f
Mykola Balabash <balabash00@gmail.com>
2019-06-13 12:05:15 +0300

    Resolved #40. Fixed bug with anchor menu links

--- js/dist/glazed.js
+++ js/dist/glazed.js
@@ -299,15 +299,28 @@ function glazedMenuGovernor(context) {
               itemsDelayInterval : 10, // delay between each menu item sliding animation
               // onItemClick: loadDummyData // callback: item that doesn´t have a submenu gets clicked - onItemClick([event], [inner HTML of the clicked item])
           });
-      // mobile menu toggle
-      $('#glazed-menu-toggle').once('glazedMenuToggle').click(function() {
-        $(this).toggleClass( 'navbar-toggle--active' );
+
+      // Close/open menu function
+      var closeMenu = function () {
+        $('#glazed-menu-toggle').toggleClass( 'navbar-toggle--active' );
         $(menuEl).toggleClass( 'menu--open' );
         $('html').toggleClass( 'html--glazed-nav-mobile--open' );
+      };
+
+      // mobile menu toggle
+      $('#glazed-menu-toggle').once('glazedMenuToggle').click(function() {
+        closeMenu();
       });
       $('#glazed-main-menu').show();
 
-      // See if logo  or block content overlaps menu and apply correction
+      // Close menu with click on anchor link
+      $('.menu__link').click(function () {
+        if (!$(this).attr('data-submenu')) {
+          closeMenu();
+        }
+      });
+
+    // See if logo  or block content overlaps menu and apply correction
       if ($('.wrap-branding').length > 0) {
         var brandingBottom = $('.wrap-branding')[0].getBoundingClientRect().bottom;
       }
