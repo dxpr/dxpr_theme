a98a3db909866b8b2a4726486bc33d30c4208f51
Mykola Balabash <balabash00@gmail.com>
2019-07-18 12:54:14 +0300

    Issue #46 Added support for throttle function if underscore js is not found.

--- js/dist/glazed.js
+++ js/dist/glazed.js
@@ -101,7 +101,7 @@ Drupal.behaviors.glazed = {
   }
 };
 
-// Create underscore debounce function if it doesn't exist already
+// Create underscore debounce and throttle functions if they doesn't exist already
 if(typeof _ != 'function'){
   window._ = {};
   window._.debounce = function(func, wait, immediate) {
@@ -159,6 +159,37 @@ if(typeof _ != 'function'){
     }, wait);
   });
 
+  window._.throttle = function (func, wait, options) {
+    var context, args, result;
+    var timeout = null;
+    var previous = 0;
+    if (!options) options = {};
+    var later = function () {
+      previous = options.leading === false ? 0 : _.now();
+      timeout = null;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    };
+    return function () {
+      var now = _.now();
+      if (!previous && options.leading === false) previous = now;
+      var remaining = wait - (now - previous);
+      context = this;
+      args = arguments;
+      if (remaining <= 0 || remaining > wait) {
+        if (timeout) {
+          clearTimeout(timeout);
+          timeout = null;
+        }
+        previous = now;
+        result = func.apply(context, args);
+        if (!timeout) context = args = null;
+      } else if (!timeout && options.trailing !== false) {
+        timeout = setTimeout(later, remaining);
+      }
+      return result;
+    };
+  };
 }
 
 $(window).resize(_.debounce(function(){
