86d21c6c7dc4ca93de593dd91ee6214fd9c0f04a
Mykola Balabash <balabash00@gmail.com>
2019-07-23 10:50:17 +0300

    Fixed #46 Added logic for smooth scroll

--- js/dist/glazed.js
+++ js/dist/glazed.js
@@ -111,9 +111,42 @@ Drupal.behaviors.glazed = {
   }
 };
 
-// Create underscore debounce function if it doesn't exist already
+// Create underscore debounce and throttle functions if they doesn't exist already
 if(typeof _ != 'function'){
   window._ = {};
+
+  window._.throttle = function (func, wait, options) {
+    var context, args, result;
+    var timeout = null;
+    var previous = 0;
+    if (!options) options = {};
+    var later = function () {
+      previous = options.leading === false ? 0 : _.now();
+      timeout = null;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    };
+    return function () {
+      var now = _.now();
+      if (!previous && options.leading === false) previous = now;
+      var remaining = wait - (now - previous);
+      context = this;
+      args = arguments;
+      if (remaining <= 0 || remaining > wait) {
+        if (timeout) {
+          clearTimeout(timeout);
+          timeout = null;
+        }
+        previous = now;
+        result = func.apply(context, args);
+        if (!timeout) context = args = null;
+      } else if (!timeout && options.trailing !== false) {
+        timeout = setTimeout(later, remaining);
+      }
+      return result;
+    };
+  };
+
   window._.debounce = function(func, wait, immediate) {
     var timeout, result;
 
@@ -180,6 +213,26 @@ $(window).resize(_.debounce(function(){
     $('.glazed-main-menu').removeClass('glazed-main-menu--to-left');
   }
 }, 50));
+
+$(document).ready(function () {
+  var navBreak = 'glazedNavBreakpoint' in window ? window.glazedNavBreakpoint : 1200;
+  if ($('.glazed-header--sticky').length > 0 && $(window).width() > navBreak) {
+    var headerHeight = Drupal.settings.glazed.headerHeight;
+    var headerScroll = Drupal.settings.glazed.headerOffset;
+    var scroll = 0;
+
+    if (headerHeight && headerScroll) {
+      _.throttle($(window).scroll(function () {
+        scroll = $(window).scrollTop();
+        if (scroll >= headerScroll && scroll <= headerScroll * 2) {
+          document.getElementsByClassName("wrap-containers")[0].style.cssText = "margin-top:" + +headerHeight + "px";
+        } else if (scroll < headerScroll) {
+          document.getElementsByClassName("wrap-containers")[0].style.cssText = "margin-top:0";
+        }
+      }), 100);
+    }
+  }
+});
 
 // Mobile menu open direction.
 $(document).ready(function () {
