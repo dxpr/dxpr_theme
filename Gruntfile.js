fe20244039fdb2b50c5883f5eeecc6f9cc51c42c
Jurriaan Roelofs <info@jurr.me>
2019-10-04 11:51:16 +0200

    update node packages and Gruntfile configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
     },
     postcss: {
         options: {
-            processors: require('autoprefixer')({browsers: ['last 2 versions', 'ie 9']}),
+            processors: require('autoprefixer'),
         },
         dist: {
             src: 'css/*.css',
