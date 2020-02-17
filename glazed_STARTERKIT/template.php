1cf67b3bb5a4edb90aebe88e33f4e52b9cdfdddd
Denis <dench0@yandex.ru>
2020-02-17 14:53:52 +0200

    Split glazed.css file into smaller parts

--- glazed_STARTERKIT/template.php
+++ glazed_STARTERKIT/template.php
@@ -4,14 +4,13 @@
  * template.php
  */
 
-
 /**
- * Override or insert variables into the page template for HTML output.
+ * Implements hook_preprocess_HOOK().
  */
-function glazed_STARTERKIT_process_html(&$vars) {
-  // Hook into color.module.
-  if (module_exists('color')) {
-    _color_html_alter($vars);
+function glazed_STARTERKIT_preprocess_page(&$variables) {
+  $path = drupal_get_path('theme', 'glazed_STARTERKIT') . '/css/';
+  foreach (_glazed_get_stylesheets() as $stylesheet) {
+    drupal_add_css($path . $stylesheet, array('group' => CSS_THEME));
   }
 }
 
--- template.php
+++ template.php
@@ -42,3 +42,75 @@ function glazedNavFastInit() {
 EOT;
   return $nav_init;
 }
+
+/**
+ * Return all styles files names.
+ *
+ * @return array
+ *  Array of all glazed theme style files names.
+ */
+function _glazed_get_stylesheets() {
+  return array(
+    // bootstrap TAILORING
+    'bootstrap-3.css',
+    'bootstrap-theme.css',
+    'jquery-ui.css',
+    // GLAZED BASE
+    'footer-menu.css',
+    'forms.css',
+    'layout.css',
+    'page-title.css',
+    'typography.css',
+    // GLAZED Components
+    'stpe-buttons.css',
+    'glazed-full-screen-search.css',
+    'glazed-header.css',
+    'glazed-header--top.css',
+    'glazed-header--side.css',
+    'glazed-header--mobile.css',
+    'glazed-secondary-header.css',
+    // Overrides
+    'cubeportfolio.css',
+    'drupal-breadcrumbs.css',
+    'drupal-comments.css',
+    'drupal-media.css',
+    'drupal-pager.css',
+    'drupal-webform.css',
+    'cms-bootstrap-core.css',
+    'cms-bootstrap-blog.css',
+    'cms-bootstrap-events.css',
+    'cms-bootstrap-news.css',
+    'cms-bootstrap-portfolio.css',
+    'sooperthemes-premium-elements.css',
+    'revolution-slider-4.css',
+    'glazed-builder.css',
+    // HELPERS
+    'helper-classes.css',
+  );
+}
+
+/**
+ * Replace stylesheet by the colorized version.
+ *
+ * @param array $vars
+ *  Variables array.
+ */
+function _glazed_color_html_alter(&$vars) {
+  global $theme_key;
+  $theme_path = drupal_get_path('theme', $theme_key);
+  // Override stylesheets.
+  $color_paths = variable_get('color_' . $theme_key . '_stylesheets', array());
+  if (!empty($color_paths)) {
+    $color_paths_map = array();
+    foreach ($color_paths as $color_path) {
+      $color_paths_map[drupal_basename($color_path)] = $color_path;
+    }
+    foreach ($vars['css'] as $old_path => $data) {
+      if (strpos($old_path, $theme_path) === 0 && isset($color_paths_map[drupal_basename($old_path)])) {
+        $vars['css'][$old_path]['data'] = $color_paths_map[drupal_basename($old_path)];
+      }
+    }
+
+    $vars['styles'] = drupal_get_css($vars['css']);
+  }
+}
