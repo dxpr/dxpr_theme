0604a0d34c4a298d6d26091610aa658864ab6610
Denis <dench0@yandex.ru>
2020-02-20 17:14:50 +0200

    Remove css files loading from subtheme preprocess

--- glazed_STARTERKIT/template.php
+++ glazed_STARTERKIT/template.php
@@ -5,16 +5,6 @@
  */
 
 /**
- * Implements hook_preprocess_HOOK().
- */
-function glazed_STARTERKIT_preprocess_page(&$variables) {
-  $path = drupal_get_path('theme', 'glazed_STARTERKIT') . '/css/';
-  foreach (_glazed_get_stylesheets() as $stylesheet) {
-    drupal_add_css($path . $stylesheet, array('group' => CSS_THEME));
-  }
-}
-
-/**
  * Override or insert variables into the page template.
  */
 function glazed_STARTERKIT_process_page(&$vars) {
