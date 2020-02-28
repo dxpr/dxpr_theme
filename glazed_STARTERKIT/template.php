835326e6a7638bffec77b55f690ba3ab48b6db6c
Denis <dench0@yandex.ru>
2020-02-28 08:54:09 +0200

    Fix: remove root scss files

--- glazed_STARTERKIT/template.php
+++ glazed_STARTERKIT/template.php
@@ -3,13 +3,3 @@
  * @file
  * template.php
  */
-
-/**
- * Override or insert variables into the page template.
- */
-function glazed_STARTERKIT_process_page(&$vars) {
-  // Hook into color.module.
-  if (module_exists('color')) {
-    _color_page_alter($vars);
-  }
-}
