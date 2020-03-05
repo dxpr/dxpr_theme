7e3a355abd47cecbcfea68ab7d9705d495e5065b
Jurriaan Roelofs <info@jurr.me>
2020-03-05 12:20:07 +0100

    disable sourceMap

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
     },
     sass: {
       options: {
-        sourceMap: true,
+        sourceMap: false,
         outputStyle:'compressed'
       },
       dist: {
