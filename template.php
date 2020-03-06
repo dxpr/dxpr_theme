32be6670244143b8b8515cc669257319a5bec8a1
Jurriaan Roelofs <info@jurr.me>
2020-03-06 15:06:47 +0100

    load css files conditionally

--- template.php
+++ template.php
@@ -43,43 +43,6 @@ EOT;
   return $nav_init;
 }
 
-/**
- * Return all styles files names.
- *
- * @return array
- *  Array of all glazed theme style files names.
- */
-function _glazed_sitewide_css() {
-  return array(
-    // Overrides
-    'vendor-extensions/bootstrap-3.css',
-    'vendor-extensions/bootstrap-theme.css',
-    'vendor-extensions/jquery-ui.css',
-    // GLAZED BASE
-    'base/footer-menu.css',
-    'base/forms.css',
-    'base/layout.css',
-    'base/page-title.css',
-    'base/typography.css',
-    // GLAZED Components
-    'components/glazed-full-screen-search.css',
-    'components/glazed-header.css',
-    'components/glazed-header--top.css',
-    'components/glazed-header--side.css',
-    'components/glazed-header--mobile.css',
-    'components/glazed-secondary-header.css',
-    'components/glazed-sidebar--menu.css',
-    // Overrides
-    'vendor-extensions/drupal-breadcrumbs.css',
-    'vendor-extensions/drupal-comments.css',
-    'vendor-extensions/drupal-media-browser.css',
-    'vendor-extensions/drupal-pager.css',
-    'vendor-extensions/drupal-mini-pager.css',
-    'vendor-extensions/drupal-prevnext.css',
-    // HELPERS
-    'helpers/helper-classes.css',
-  );
-}
 
 /**
  * Replace stylesheet by the colorized version.
