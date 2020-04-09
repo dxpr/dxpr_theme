ee9147defebf76abdbc3d7bad23d62952cde2e9d
Jurriaan Roelofs <info@jurr.me>
2020-04-09 14:18:27 +0200

    remove depracated files from Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,10 +8,8 @@ module.exports = function(grunt) {
       dist: {
         files: {
           'js/minified/glazed.min.js': ['js/dist/glazed.js'],
-          'js/minified/glazed.admin.min.js': ['js/dist/glazed.admin.js'],
           'js/minified/glazed-settings.admin.min.js': ['js/dist/glazed-settings.admin.js'],
           'js/minified/glazed-mobile-nav.min.js': ['js/dist/glazed-mobile-nav.js'],
-          'js/minified/glazed-ilightbox.min.js': ['js/dist/glazed-ilightbox.js'],
           'js/minified/classie.min.js': ['vendor/classie.js']
         }
       }
