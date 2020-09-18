80f85937ccfcbed4f940aa464b2875b858cdfd6c
Jurriaan Roelofs <info@jurr.me>
2020-09-18 15:38:58 +0200

    working on scripts

--- scripts/create_subtheme.sh
+++ scripts/create_subtheme.sh
@@ -3,27 +3,34 @@
 
 echo '
 +------------------------------------------------------------------------+
-| With this script you could quickly create DXPR sub-theme               |
+| With this script you can quickly create a DXPR sub-theme               |
 +------------------------------------------------------------------------+
 '
+
+foldername="$(basename $PWD)"
 parentfoldername="$(basename "$(dirname $PWD)")"
-echo "${DIRECTORY}"
-if [ "${parentfoldername}" != "contrib" ]; then
-  echo 'Error: The dxpr_theme theme (this folder) should be in the "contrib" folder!'
+
+if [ "${foldername}" != "dxpr_theme" ]; then
+  echo 'Error: This command should be run from the DXPR Theme root folder (themes/contrib/dxpr_theme).'
   exit
 fi
 
-echo 'You must enter  name! [e.g. My Custom DXPR Theme]'
+if [ "${parentfoldername}" != "contrib" ]; then
+  echo 'Error: The dxpr_theme theme (this folder) should be in the "contrib" folder.'
+  exit
+fi
+
+echo 'Please enter the name for your theme [e.g. My Custom DXPR Theme]'
 read CUSTOM_DXPR_THEME_NAME
 if [ -z "${CUSTOM_DXPR_THEME_NAME}" ]; then
-  echo 'Error: You must enter name! [e.g. My Custom DXPR Theme]'
+  echo 'Error: Please enter a name [e.g. My Custom DXPR Theme]'
   exit
 fi
 
-echo 'The machine name of your custom theme? [e.g. my_custom_dxpr_theme]'
+echo 'Please enter the machine name for your theme [e.g. my_custom_dxpr_theme]'
 read CUSTOM_DXPR_THEME
 if [ -z "${CUSTOM_DXPR_THEME}" ]; then
-  echo 'Error: You must enter machine name of your custom theme? [e.g. my_custom_dxpr_theme]'
+  echo 'Error: Please enter a machine name [e.g. my_custom_dxpr_theme]'
   exit
 fi
 
@@ -39,8 +46,9 @@ for file in config/*/*dxpr_theme_STARTERKIT.*; do mv $file ${file//dxpr_theme_ST
 # Difference of commands is i ''
 if [[ "$OSTYPE" == "darwin"* ]]; then
   grep -Rl dxpr_theme_STARTERKIT . | xargs sed -i '' -e "s/dxpr_theme_STARTERKIT/$CUSTOM_DXPR_THEME/"
+  sed -i '' -e "s/THEMETITLE/$CUSTOM_DXPR_THEME_NAME/" $CUSTOM_DXPR_THEME.info.yml config/schema/$CUSTOM_DXPR_THEME.schema.yml
 else
   grep -Rl dxpr_theme_STARTERKIT . | xargs sed -i -e "s/dxpr_theme_STARTERKIT/$CUSTOM_DXPR_THEME/"
+  sed -i -e "s/THEMETITLE/$CUSTOM_DXPR_THEME_NAME/" $CUSTOM_DXPR_THEME.info.yml config/schema/$CUSTOM_DXPR_THEME.schema.yml
 fi
-sed -i -e "s/THEMETITLE/$CUSTOM_DXPR_THEME_NAME/" $CUSTOM_DXPR_THEME.info.yml config/schema/$CUSTOM_DXPR_THEME.schema.yml
 echo "# Check the themes/custom folder for your new sub-theme."
