5eeb292448fe1d64495046d3475a2c431b706e51
jjroelofs <904576+jjroelofs@users.noreply.github.com>
2023-06-26 08:56:38 +0200

    Jur/5.x/#432 facelift dxpr theme settings form (#433)
    * remove unused variables, improve variable naming
    
    * facelift
    
    * make hovered vert menus items blue to match gin theme hover styling
    
    ---------
    
    Co-authored-by: Jurriaan Roelofs <jur@dxpr.com>

--- theme-settings.php
+++ theme-settings.php
@@ -50,7 +50,7 @@ function dxpr_theme_form_system_theme_settings_alter(&$form, &$form_state, $form
   $dxpr_theme_theme_path = \Drupal::service('extension.list.theme')->getPath('dxpr_theme') . '/';
   $themes = \Drupal::service('theme_handler')->listInfo();
 
-  $img = '<img width="100" height="37" src="' . $base_path . $dxpr_theme_theme_path . 'dxpr-logo-white.svg" />';
+  $img = '<img width="40" height="15" src="' . $base_path . $dxpr_theme_theme_path . 'images/dxpr-logo-dark.svg" />';
   if (!empty($themes[$subject_theme]->info['version'])) {
     $version = $themes[$subject_theme]->info['version'];
   }
@@ -62,7 +62,7 @@ function dxpr_theme_form_system_theme_settings_alter(&$form, &$form_state, $form
     // STOPS RENDERING OF ALL ELEMENTS INSIDE.
     '#type' => 'vertical_tabs',
     '#weight' => -20,
-    '#prefix' => '<h2><small>' . $img . ' ' . ucfirst($subject_theme) . ' ' . $version . ' <span class="small">(' . $themes['bootstrap5']->info['name'] . ' base theme ' . $themes['bootstrap5']->info['version'] . ')</span>' . '</small></h2>',
+    '#prefix' => '<h2><small>' . $img . ' ' . $themes[$subject_theme]->info['name'] . ' ' . $version . ' <span class="small">(' . $themes['bootstrap5']->info['name'] . ' base theme ' . $themes['bootstrap5']->info['version'] . ')</span>' . '</small></h2>',
   ];
   // $form['color']['#group'] = 'dxpr_theme_settings';
   if (!empty($form['update'])) {
