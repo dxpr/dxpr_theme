cd7693676e6245f3ff7a12124816330cf63d2e1d
dxc <679028+deuxcode@users.noreply.github.com>
2023-11-13 18:57:16 +0100

    Update theme post_update hook to support multiple sub-themes
    In addition the Color modules is now also uninstalled after the
    colors have been migrated.
    
    modified: dxpr_theme.post_update.php

--- dxpr_theme.post_update.php
+++ dxpr_theme.post_update.php
@@ -9,33 +9,46 @@
  * Migrate colors from color module to theme settings.
  */
 function dxpr_theme_post_update_n1_migrate_colors() {
-  /** @var Drupal\Core\Extension\ThemeHandler $theme_handler */
+  /** @var \Drupal\Core\Extension\ThemeHandler $theme_handler */
   $theme_handler = \Drupal::service('theme_handler');
-  $theme_name = $theme_handler->getDefault();
+  $theme_list = $theme_handler->listInfo();
 
-  $config = \Drupal::configFactory()->getEditable($theme_name . '.settings');
-
-  // Get color module palette.
+  // Load Color module.
   \Drupal::moduleHandler()->loadInclude('module', 'color');
-  $color_palette = color_get_palette($theme_name);
 
-  $config->set('color_scheme', 'custom');
-  $config->set('color_palette', serialize($color_palette));
-
-  foreach ($color_palette as $name => $clr) {
-    $config->set('color_palette_' . $name, $clr);
-  }
-
-  $config->save();
-
-  // Rebuild theme CSS.
+  // Load callbacks.
   require_once $theme_handler
     ->getTheme('dxpr_theme')
     ->getPath() . '/dxpr_theme_callbacks.inc';
 
-  if (function_exists('dxpr_theme_css_cache_build')) {
-    dxpr_theme_css_cache_build($theme_name);
+  /** @var \Drupal\Core\Extension\Extension $theme */
+  foreach ($theme_list as $theme) {
+    if ('dxpr_theme' === ($theme->info['base theme'] ?? '')) {
+      $theme_name = $theme->getName();
+
+      $config = \Drupal::configFactory()
+        ->getEditable($theme_name . '.settings');
+
+      // Get color module palette.
+      $color_palette = color_get_palette($theme_name);
+      $config->set('color_scheme', 'custom');
+      $config->set('color_palette', serialize($color_palette));
+
+      foreach ($color_palette as $name => $clr) {
+        $config->set('color_palette_' . $name, $clr);
+      }
+
+      $config->save();
+
+      // Rebuild theme CSS.
+      if (function_exists('dxpr_theme_css_cache_build')) {
+        dxpr_theme_css_cache_build($theme_name);
+      }
+    }
   }
 
-  return t('The theme color settings have been migrated. You can now safely uninstall the Color module unless used elsewhere.');
+  // Uninstall the Color module.
+  \Drupal::service('module_installer')->uninstall(['color']);
+
+  return t('The theme color settings have been migrated, and the Color module has been uninstalled.');
 }
