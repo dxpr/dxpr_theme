7ec15eeb151d61c92f30002d34519e84757c5e1a
drale1 <drazenmusa@yahoo.com>
2024-11-05 10:58:49 +0100

    Extracted menu-mobile.js

--- js/dist/header/dxpr-theme-header.js
+++ js/dist/header/dxpr-theme-header.js
@@ -8,6 +8,7 @@
 const { setupStickyHeader } = require("./sticky-header");
 const { debounce, throttle, delay } = require("./performance-helpers");
 const { setupDesktopMenu } = require("./menu-desktop");
+const { setupMobileMenu } = require("./menu-mobile");
 
 (function (Drupal, once) {
   let dxpr_themeMenuState = "";
@@ -125,116 +126,10 @@ const { setupDesktopMenu } = require("./menu-desktop");
         return false;
       }
 
-      // Temporary hiding while settings up @see #290
-      document.getElementById("dxpr-theme-main-menu").style.display = "none";
+      //Injecting menu-mobile.js
+      setupMobileMenu();
 
-      // Set up classes
-      const headerTop = document.querySelector(".dxpr-theme-header--top");
-      if (headerTop) {
-        headerTop.classList.add("dxpr-theme-header--side");
-        headerTop.classList.remove("dxpr-theme-header--top");
-      }
-
-      // Remove split-mega menu columns
-      document
-        .querySelectorAll(
-          "#dxpr-theme-main-menu .menu .dropdown-menu, #dxpr-theme-main-menu .menu .dropdown-menu li",
-        )
-        .forEach((el) => {
-          el.removeAttribute("style");
-        });
-
-      const mainMenu = document.getElementById("dxpr-theme-main-menu");
-      if (mainMenu) {
-        const menuItems = mainMenu.querySelectorAll(".menu");
-        menuItems.forEach((menuItem) => {
-          menuItem.classList.add("menu__level");
-
-          const dropdownMenus = menuItem.querySelectorAll(".dropdown-menu");
-          dropdownMenus.forEach((dropdownMenu) => {
-            dropdownMenu.classList.add("menu__level");
-          });
-
-          const megamenus = menuItem.querySelectorAll(".dxpr-theme-megamenu");
-          megamenus.forEach((megamenu) => {
-            megamenu.classList.add("menu__level");
-          });
-
-          const links = menuItem.querySelectorAll("a");
-          links.forEach((link) => {
-            link.classList.add("menu__link");
-          });
-
-          const listItems = menuItem.querySelectorAll("li");
-          listItems.forEach((listItem) => {
-            listItem.classList.add("menu__item");
-          });
-        });
-      }
-
-      // Set up data attributes
-      document
-        .querySelectorAll("#dxpr-theme-main-menu .menu a.dropdown-toggle")
-        .forEach((element) => {
-          const nextElement = element.nextElementSibling;
-          element.setAttribute("data-submenu", element.textContent);
-          nextElement.setAttribute("data-menu", element.textContent);
-        });
-
-      document
-        .querySelectorAll(
-          "#dxpr-theme-main-menu .menu a.dxpr-theme-megamenu__heading",
-        )
-        .forEach((element) => {
-          const nextMegaElement = element.nextElementSibling;
-          element.setAttribute("data-submenu", element.textContent);
-          nextMegaElement.setAttribute("data-menu", element.textContent);
-        });
-
-      const bc =
-        document.querySelectorAll("#dxpr-theme-main-menu .menu .dropdown-menu")
-          .length > 0;
-      const menuEl = document.getElementById("dxpr-theme-main-menu");
-      new MLMenu(menuEl, {
-        breadcrumbsCtrl: bc, // Show breadcrumbs
-        initialBreadcrumb: "menu", // Initial breadcrumb text
-        backCtrl: false, // Show back button
-        itemsDelayInterval: 10, // Delay between each menu item sliding animation
-      });
-
-      // Close/open menu function
-      const closeMenu = function () {
-        if (drupalSettings.dxpr_themeSettings.hamburgerAnimation === "cross") {
-          document
-            .querySelector("#dxpr-theme-menu-toggle")
-            .classList.toggle("navbar-toggle--active");
-        }
-        document
-          .querySelector("#dxpr-theme-main-menu")
-          .classList.toggle("menu--open");
-        document
-          .querySelector("html")
-          .classList.toggle("html--dxpr-theme-nav-mobile--open");
-      };
-
-      // Mobile menu toggle
-      document
-        .querySelector("#dxpr-theme-menu-toggle")
-        .addEventListener("click", () => {
-          closeMenu();
-        });
-
-      document.getElementById("dxpr-theme-main-menu").style.position = "fixed";
-      document.getElementById("dxpr-theme-main-menu").style.display = "block";
-
-      // Close menu with click on anchor link
-      document.querySelectorAll(".menu__link").forEach((link) => {
-        link.addEventListener("click", function () {
-          if (!this.getAttribute("data-submenu")) {
-            closeMenu();
-          }
-        });
-      });
+      dxpr_themeMenuState = "side"
 
       // See if logo or block content overlaps menu and apply correction
       let brandingBottom = 0;
