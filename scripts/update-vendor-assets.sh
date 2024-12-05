ca4ad6f9e028c794a9916f12fcff2d25d59a790d
drale1 <drazenmusa@yahoo.com>
2024-12-05 17:09:19 +0100

    Updated dxb-slider to version 2

--- scripts/update-vendor-assets.sh
+++ scripts/update-vendor-assets.sh
@@ -1,28 +1,19 @@
 #!/bin/bash
 
-echo "Updating DXB Slider package with npm..."
-npm update dxb_slider
+echo "Updating vendor packages with npm..."
+npm update dxb_slider reinvented-color-wheel
 
-SLIDER_JS_SRC="./node_modules/dxb_slider/dxb-slider.min.js"
-SLIDER_CSS_SRC="./node_modules/dxb_slider/dxb-slider.min.css"
-VENDOR_SLIDER_JS_DEST="./vendor/dxb-slider/dxb-slider.min.js"
-VENDOR_SLIDER_CSS_DEST="./vendor/dxb-slider/dxb-slider.min.css"
+# Map of source directories to destination directories
+declare -A packages=(
+  ["./node_modules/dxb_slider"]="vendor/dxb-slider"
+  ["./node_modules/reinvented-color-wheel"]="vendor/color-wheel"
+)
 
-COLOR_WHEEL_JS_SRC="./node_modules/reinvented-color-wheel/iife/reinvented-color-wheel.min.js"
-COLOR_WHEEL_CSS_SRC="./node_modules/reinvented-color-wheel/css/reinvented-color-wheel.min.css"
-VENDOR_COLOR_WHEEL_JS_DEST="./vendor/color-wheel/reinvented-color-wheel.min.js"
-VENDOR_COLOR_WHEEL_CSS_DEST="./vendor/color-wheel/reinvented-color-wheel.min.css"
-
-echo "Creating vendor directories if they do not exist..."
-mkdir -p "$(dirname "$VENDOR_SLIDER_JS_DEST")"
-mkdir -p "$(dirname "$VENDOR_COLOR_WHEEL_JS_DEST")"
-
-echo "Copying DXB Slider files to vendor folder..."
-cp "$SLIDER_JS_SRC" "$VENDOR_SLIDER_JS_DEST" || echo "Warning: $SLIDER_JS_SRC not found"
-cp "$SLIDER_CSS_SRC" "$VENDOR_SLIDER_CSS_DEST" || echo "Warning: $SLIDER_CSS_SRC not found"
-
-echo "Copying Reinvented Color Wheel files to vendor folder..."
-cp "$COLOR_WHEEL_JS_SRC" "$VENDOR_COLOR_WHEEL_JS_DEST" || echo "Warning: $COLOR_WHEEL_JS_SRC not found"
-cp "$COLOR_WHEEL_CSS_SRC" "$VENDOR_COLOR_WHEEL_CSS_DEST" || echo "Warning: $COLOR_WHEEL_CSS_SRC not found"
+echo "Creating vendor directories and copying packages..."
+for src in "${!packages[@]}"; do
+  dest="${packages[$src]}"
+  mkdir -p "$dest"
+  cp -r "$src"/* "$dest" || echo "Warning: Failed to copy files from $src to $dest"
+done
 
 echo "Vendor assets updated successfully."
