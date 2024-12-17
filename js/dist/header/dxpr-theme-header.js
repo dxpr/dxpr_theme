90fb74695f3aac42a1473026675292288c0fad38
drale1 <drazenmusa@yahoo.com>
2024-12-17 09:11:21 +0100

    Delete WCAG luminance formula

--- js/dist/header/dxpr-theme-header.js
+++ js/dist/header/dxpr-theme-header.js
@@ -15,7 +15,6 @@ const { adjustMenuPosition } = require("./menu-position");
 const { applyFixedHeaderStyles } = require("./apply-fixed-header-styles");
 const { dxpr_themeMenuGovernorBodyClass } = require("./menu-governor-body");
 const { dxpr_themeMenuOnResize } = require("./menu-resize-handler");
-const { setupDarkBackgroundMenu } = require("./dark-background-menu");
 
 (function (Drupal, once) {
   let dxpr_themeMenuState = "";
@@ -127,6 +126,4 @@ const { setupDarkBackgroundMenu } = require("./dark-background-menu");
       dxpr_themeMenuGovernor(document);
     }
   });
-
-  setupDarkBackgroundMenu();
 })(Drupal, once);
