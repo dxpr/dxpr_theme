9552fd767dcf7c03f5f6f11953f9625d50e9096b
Jurriaan Roelofs <jur@dxpr.com>
2025-08-19 20:01:03 +0200

    lint

--- theme-settings.php
+++ theme-settings.php
@@ -12,29 +12,28 @@ use Drupal\node\Entity\NodeType;
 /**
  * Implements hook_form_FORM_ID_alter().
  */
-function dxpr_theme_form_system_theme_settings_alter(&$form, &$form_state, $form_id = null)
-{
-    global $base_path;
+function dxpr_theme_form_system_theme_settings_alter(&$form, &$form_state, $form_id = NULL) {
+  global $base_path;
   // @code
   // A bug in D7 and D8 causes the theme to load twice.
   // Only the second time $form
   // will contain the color module data. So we ignore the first
   // @see https://www.drupal.org/project/drupal/issues/943212#comment-12102383
   // form_id is only present the second time around.
-    if (!isset($form_id)) {
-        return;
-    }
+  if (!isset($form_id)) {
+    return;
+  }
 
-    $build_info = $form_state->getBuildInfo();
-    $subject_theme = $build_info['args'][0];
-    $dxpr_theme_theme_path = \Drupal::service('extension.list.theme')->getPath('dxpr_theme') . '/';
-    $themes = \Drupal::service('theme_handler')->listInfo();
+  $build_info = $form_state->getBuildInfo();
+  $subject_theme = $build_info['args'][0];
+  $dxpr_theme_theme_path = \Drupal::service('extension.list.theme')->getPath('dxpr_theme') . '/';
+  $themes = \Drupal::service('theme_handler')->listInfo();
 
-    if (!empty($themes[$subject_theme]->info['version'])) {
-        $version = $themes[$subject_theme]->info['version'];
-    }
+  if (!empty($themes[$subject_theme]->info['version'])) {
+    $version = $themes[$subject_theme]->info['version'];
+  }
 
-    $form['dxpr_theme_settings_header'] = [
+  $form['dxpr_theme_settings_header'] = [
     '#type' => 'inline_template',
     '#template' => '
       <div class="form-header">
@@ -56,20 +55,20 @@ function dxpr_theme_form_system_theme_settings_alter(&$form, &$form_state, $form
       'preview_text' => t('No preview. Save to view changes.'),
     ],
     '#weight' => -100,
-    ];
+  ];
 
-    $form['dxpr_theme_settings'] = [
+  $form['dxpr_theme_settings'] = [
     // SETTING TYPE TO DETAILS OR VERTICAL_TABS
     // STOPS RENDERING OF ALL ELEMENTS INSIDE.
     '#type' => 'vertical_tabs',
     '#weight' => -20,
-    ];
+  ];
 
-    if (!empty($form['update'])) {
-        $form['update']['#group'] = 'global';
-    }
+  if (!empty($form['update'])) {
+    $form['update']['#group'] = 'global';
+  }
 
-    $form['core_theme_settings_header'] = [
+  $form['core_theme_settings_header'] = [
     '#type' => 'inline_template',
     '#template' => '
       <div class="form-header">
@@ -80,9 +79,9 @@ function dxpr_theme_form_system_theme_settings_alter(&$form, &$form_state, $form
       'title' => t('Core theme settings'),
     ],
     '#weight' => -10,
-    ];
+  ];
 
-    $form['core_theme_settings'] = [
+  $form['core_theme_settings'] = [
     '#type' => 'vertical_tabs',
     '#weight' => 0,
     '#attributes' => [
@@ -90,21 +89,21 @@ function dxpr_theme_form_system_theme_settings_alter(&$form, &$form_state, $form
         'core-theme-settings',
       ],
     ],
-    ];
-    $form['theme_settings']['#group'] = 'core_theme_settings';
-    $form['logo']['#group'] = 'core_theme_settings';
-    $form['favicon']['#group'] = 'core_theme_settings';
+  ];
+  $form['theme_settings']['#group'] = 'core_theme_settings';
+  $form['logo']['#group'] = 'core_theme_settings';
+  $form['favicon']['#group'] = 'core_theme_settings';
 
-  // Web Icons group
-    $form['web_icons'] = [
+  // Web Icons group.
+  $form['web_icons'] = [
     '#type' => 'details',
     '#title' => t('Web/App Icons'),
     '#description' => t('Configure icons for mobile devices and progressive web apps.'),
     '#group' => 'core_theme_settings',
-    ];
+  ];
 
-  // Single web icons upload
-    $form['web_icons_upload'] = [
+  // Single web icons upload.
+  $form['web_icons_upload'] = [
     '#type' => 'media_library',
     '#title' => t('Web Icons'),
     '#description' => t('Upload an icon that will be automatically resized for iOS, Android, and PWA use. Recommended minimum size: 512x512 pixels.'),
@@ -112,13 +111,13 @@ function dxpr_theme_form_system_theme_settings_alter(&$form, &$form_state, $form
     '#default_value' => theme_get_setting('web_icons_upload'),
     '#cardinality' => 1,
     '#group' => 'web_icons',
-    ];
-    unset($form['body_details']);
-    unset($form['nav_details']);
-    unset($form['footer_details']);
-    unset($form['subtheme']);
-    unset($form['styleguide']);
-    unset($form['text_formats']);
+  ];
+  unset($form['body_details']);
+  unset($form['nav_details']);
+  unset($form['footer_details']);
+  unset($form['subtheme']);
+  unset($form['styleguide']);
+  unset($form['text_formats']);
 
   /**
    * DXPR Theme cache builder
@@ -130,27 +129,27 @@ function dxpr_theme_form_system_theme_settings_alter(&$form, &$form_state, $form
    * settings are saved and once after the redirect with the updated settings.
    * @todo come up with a less 'icky' solution
    */
-    require_once \Drupal::service('extension.list.theme')->getPath('dxpr_theme') . '/dxpr_theme_callbacks.inc';
+  require_once \Drupal::service('extension.list.theme')->getPath('dxpr_theme') . '/dxpr_theme_callbacks.inc';
 
-    $dxpr_theme_css_file = _dxpr_theme_css_cache_file($subject_theme);
-    if (!file_exists($dxpr_theme_css_file)) {
-        dxpr_theme_css_cache_build($subject_theme);
+  $dxpr_theme_css_file = _dxpr_theme_css_cache_file($subject_theme);
+  if (!file_exists($dxpr_theme_css_file)) {
+    dxpr_theme_css_cache_build($subject_theme);
+  }
+
+  foreach (\Drupal::service('file_system')->scanDirectory(\Drupal::service('extension.list.theme')->getPath('dxpr_theme') . '/features', '/settings.inc/i') as $file) {
+    require_once $file->uri;
+    $function_name = basename($file->filename, '.inc');
+    $function_name = str_replace('-', '_', $function_name);
+    if (function_exists($function_name)) {
+      $function_name($form, $subject_theme);
     }
+  }
+  $form['#attached']['library'][] = 'dxpr_theme/admin.themesettings';
 
-    foreach (\Drupal::service('file_system')->scanDirectory(\Drupal::service('extension.list.theme')->getPath('dxpr_theme') . '/features', '/settings.inc/i') as $file) {
-        require_once $file->uri;
-        $function_name = basename($file->filename, '.inc');
-        $function_name = str_replace('-', '_', $function_name);
-        if (function_exists($function_name)) {
-            $function_name($form, $subject_theme);
-        }
-    }
-    $form['#attached']['library'][] = 'dxpr_theme/admin.themesettings';
+  array_unshift($form['#submit'], 'dxpr_theme_form_system_theme_settings_submit');
+  array_unshift($form['#validate'], 'dxpr_theme_form_system_theme_settings_validate');
 
-    array_unshift($form['#submit'], 'dxpr_theme_form_system_theme_settings_submit');
-    array_unshift($form['#validate'], 'dxpr_theme_form_system_theme_settings_validate');
-
-    $form['#submit'][] = 'dxpr_theme_form_system_theme_settings_after_submit';
+  $form['#submit'][] = 'dxpr_theme_form_system_theme_settings_after_submit';
 }
 
 /**
@@ -158,37 +157,36 @@ function dxpr_theme_form_system_theme_settings_alter(&$form, &$form_state, $form
  *
  * @see \Drupal\system\Form\ThemeSettingsForm::validateForm()
  */
-function dxpr_theme_form_system_theme_settings_validate(&$form, &$form_state)
-{
-    if (\Drupal::moduleHandler()->moduleExists('media')) {
-      // If the user provided a path for a logo or background image file,
-      // make sure a file exists at that path.
-        if ($form_state->getValue('page_title_image_path')) {
-            $path = _dxpr_theme_validate_path($form_state->getValue('page_title_image_path'));
-            if (!$path) {
-                $form_state->setErrorByName('page_title_image_path', t('The custom logo path is invalid.'));
-            }
-        }
-        if ($form_state->getValue('background_image_path')) {
-            $path = _dxpr_theme_validate_path($form_state->getValue('background_image_path'));
-            if (!$path) {
-                $form_state->setErrorByName('background_image_path', t('The custom background image path is invalid.'));
-            }
-        }
+function dxpr_theme_form_system_theme_settings_validate(&$form, &$form_state) {
+  if (\Drupal::moduleHandler()->moduleExists('media')) {
+    // If the user provided a path for a logo or background image file,
+    // make sure a file exists at that path.
+    if ($form_state->getValue('page_title_image_path')) {
+      $path = _dxpr_theme_validate_path($form_state->getValue('page_title_image_path'));
+      if (!$path) {
+        $form_state->setErrorByName('page_title_image_path', t('The custom logo path is invalid.'));
+      }
     }
+    if ($form_state->getValue('background_image_path')) {
+      $path = _dxpr_theme_validate_path($form_state->getValue('background_image_path'));
+      if (!$path) {
+        $form_state->setErrorByName('background_image_path', t('The custom background image path is invalid.'));
+      }
+    }
+  }
 
   // Handle custom color validation.
   // Only accepts valid hex color values.
-    foreach ($form_state->getValues() as $key => $value) {
-        if (strpos($key, 'color_palette_') === 0) {
-            if (!preg_match('/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/', $value)) {
-                $color_names = _dxpr_theme_get_color_names();
-                $form_state->setErrorByName($key, t('The %name field contains an invalid color value.', [
-                '%name' => $color_names[str_replace('color_palette_', '', $key)] ?? t('Unknown'),
-                ]));
-            }
-        }
+  foreach ($form_state->getValues() as $key => $value) {
+    if (strpos($key, 'color_palette_') === 0) {
+      if (!preg_match('/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/', $value)) {
+        $color_names = _dxpr_theme_get_color_names();
+        $form_state->setErrorByName($key, t('The %name field contains an invalid color value.', [
+          '%name' => $color_names[str_replace('color_palette_', '', $key)] ?? t('Unknown'),
+        ]));
+      }
     }
+  }
 }
 
 /**
@@ -196,53 +194,54 @@ function dxpr_theme_form_system_theme_settings_validate(&$form, &$form_state)
  *
  * @see \Drupal\system\Form\ThemeSettingsForm::submitForm()
  */
-function dxpr_theme_form_system_theme_settings_submit(&$form, &$form_state)
-{
-    $value = $form_state->getValue('page_title_image');
-    if (!empty($value)) {
-        $media = Media::load(($value));
-        try {
-            $media_url = $media->field_media_image->entity->getFileUri();
-            $form_state->setValue('page_title_image_path', $media_url);
-        } catch (FileException $e) {
-            \Drupal::messenger()->addMessage($e->getMessage(), 'error');
-            \Drupal::logger('dxpr_theme')->error($e->getMessage());
-        }
+function dxpr_theme_form_system_theme_settings_submit(&$form, &$form_state) {
+  $value = $form_state->getValue('page_title_image');
+  if (!empty($value)) {
+    $media = Media::load(($value));
+    try {
+      $media_url = $media->field_media_image->entity->getFileUri();
+      $form_state->setValue('page_title_image_path', $media_url);
     }
+    catch (FileException $e) {
+      \Drupal::messenger()->addMessage($e->getMessage(), 'error');
+      \Drupal::logger('dxpr_theme')->error($e->getMessage());
+    }
+  }
 
-    $value = $form_state->getValue('background_image');
-    if (!empty($value)) {
-        $media = Media::load(($value));
-        try {
-            $media_url = $media->field_media_image->entity->getFileUri();
-            $form_state->setValue('background_image_path', $media_url);
-        } catch (FileException $e) {
-            \Drupal::messenger()->addMessage($e->getMessage(), 'error');
-            \Drupal::logger('dxpr_theme')->error($e->getMessage());
-        }
+  $value = $form_state->getValue('background_image');
+  if (!empty($value)) {
+    $media = Media::load(($value));
+    try {
+      $media_url = $media->field_media_image->entity->getFileUri();
+      $form_state->setValue('background_image_path', $media_url);
     }
+    catch (FileException $e) {
+      \Drupal::messenger()->addMessage($e->getMessage(), 'error');
+      \Drupal::logger('dxpr_theme')->error($e->getMessage());
+    }
+  }
 
   // If the user entered a path relative to the system files directory for
   // a logo or favicon, store a public:// URI so the theme system can handle it.
-    if (!empty($form_state->getValue('page_title_image_path'))) {
-        $path = _dxpr_theme_validate_path($form_state->getValue('page_title_image_path'));
-        $form_state->setValue('page_title_image_path', $path);
-    }
-    if (!empty($form_state->getValue('background_image_path'))) {
-        $path = _dxpr_theme_validate_path($form_state->getValue('background_image_path'));
-        $form_state->setValue('background_image_path', $path);
-    }
+  if (!empty($form_state->getValue('page_title_image_path'))) {
+    $path = _dxpr_theme_validate_path($form_state->getValue('page_title_image_path'));
+    $form_state->setValue('page_title_image_path', $path);
+  }
+  if (!empty($form_state->getValue('background_image_path'))) {
+    $path = _dxpr_theme_validate_path($form_state->getValue('background_image_path'));
+    $form_state->setValue('background_image_path', $path);
+  }
 
   // Handle color palette values.
-    $color_palette = [];
-    foreach ($form_state->getValues() as $key => $value) {
-        if (strpos($key, 'color_palette_') === 0) {
-            $hex = preg_match('/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/', $value) ? $value : '';
-            $color_palette[str_replace('color_palette_', '', $key)] = $hex;
-        }
+  $color_palette = [];
+  foreach ($form_state->getValues() as $key => $value) {
+    if (strpos($key, 'color_palette_') === 0) {
+      $hex = preg_match('/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/', $value) ? $value : '';
+      $color_palette[str_replace('color_palette_', '', $key)] = $hex;
     }
+  }
 
-    $form_state->setValue('color_palette', serialize($color_palette));
+  $form_state->setValue('color_palette', serialize($color_palette));
 }
 
 /**
@@ -251,21 +250,19 @@ function dxpr_theme_form_system_theme_settings_submit(&$form, &$form_state)
  * @return array
  *   Available node bundles.
  */
-function _dxpr_theme_node_types_options()
-{
-    $types = [];
-    foreach (NodeType::loadMultiple() as $key => $value) {
-        $types[$key] = $value->get('name');
-    }
-    return $types;
+function _dxpr_theme_node_types_options() {
+  $types = [];
+  foreach (NodeType::loadMultiple() as $key => $value) {
+    $types[$key] = $value->get('name');
+  }
+  return $types;
 }
 
 /**
  * Generate node type preview markup.
  */
-function _dxpr_theme_type_preview()
-{
-    $output = <<<EOT
+function _dxpr_theme_type_preview() {
+  $output = <<<EOT
 <div class="type-preview">
   <div class="type-container type-title-container">
     <h1>Beautiful Typography</h1>
@@ -296,7 +293,7 @@ function _dxpr_theme_type_preview()
   </div>
 </div>
 EOT;
-    return $output;
+  return $output;
 }
 
 /**
@@ -316,24 +313,23 @@ EOT;
  *
  * @see \Drupal\system\Form\ThemeSettingsForm::validatePath()
  */
-function _dxpr_theme_validate_path($path)
-{
+function _dxpr_theme_validate_path($path) {
   // Absolute local file paths are invalid.
-    if (\Drupal::service('file_system')->realpath($path) == $path) {
-        return false;
-    }
+  if (\Drupal::service('file_system')->realpath($path) == $path) {
+    return FALSE;
+  }
   // A path relative to the Drupal root or a fully qualified URI is valid.
-    if (is_file($path)) {
-        return $path;
-    }
+  if (is_file($path)) {
+    return $path;
+  }
   // Prepend 'public://' for relative file paths within public filesystem.
-    if (\Drupal::service('stream_wrapper_manager')->getScheme($path) === false) {
-        $path = 'public://' . $path;
-    }
-    if (is_file($path)) {
-        return $path;
-    }
-    return false;
+  if (\Drupal::service('stream_wrapper_manager')->getScheme($path) === FALSE) {
+    $path = 'public://' . $path;
+  }
+  if (is_file($path)) {
+    return $path;
+  }
+  return FALSE;
 }
 
 /**
@@ -343,14 +339,13 @@ function _dxpr_theme_validate_path($path)
  *
  * @see \Drupal\system\Form\ThemeSettingsForm::submitForm()
  */
-function dxpr_theme_form_system_theme_settings_after_submit(&$form, &$form_state)
-{
-    require_once \Drupal::service('extension.list.theme')->getPath('dxpr_theme') . '/dxpr_theme_callbacks.inc';
+function dxpr_theme_form_system_theme_settings_after_submit(&$form, &$form_state) {
+  require_once \Drupal::service('extension.list.theme')->getPath('dxpr_theme') . '/dxpr_theme_callbacks.inc';
 
-    $build_info = $form_state->getBuildInfo();
-    $subject_theme = $build_info['args'][0];
+  $build_info = $form_state->getBuildInfo();
+  $subject_theme = $build_info['args'][0];
   // It is needed to clear the theme cache.
-    $theme_cache =&drupal_static('theme_get_setting', []);
-    $theme_cache = [];
-    dxpr_theme_css_cache_build($subject_theme);
+  $theme_cache =&drupal_static('theme_get_setting', []);
+  $theme_cache = [];
+  dxpr_theme_css_cache_build($subject_theme);
 }
