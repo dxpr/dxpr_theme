3f0d642080fe73038f9726038e0417063128d9ca
dxc <679028+deuxcode@users.noreply.github.com>
2025-09-16 19:08:12 +0200

    Fix linting issues (#565)
    This commit fixes a multidue of linting issues for eslint,
    drupa-lint, and stylelint. The word 'Dropdowns' is now also a
    valid lint word.
    
    modified: .eslintrc
    modified: css/base/variables.css
    modified: css/components/dxpr-theme-header--mobile.css
    modified: css/components/dxpr-theme-header--top.css
    modified: js/dist/enhanced-dropdowns.js
    modified: scss/base/variables.scss
    modified: scss/components/dxpr-theme-header--mobile.scss
    modified: scss/components/dxpr-theme-header--top.scss

--- js/dist/enhanced-dropdowns.js
+++ js/dist/enhanced-dropdowns.js
@@ -1,9 +1,10 @@
+/* global BootstrapEnhancedDropdowns */
 (function (Drupal, once) {
   Drupal.behaviors.bsEnhancedDropdownInit = {
     attach(context, settings) {
-      if (once('bs-enhanced-dropdowns-init', 'html', context).length) {
+      if (once("bs-enhanced-dropdowns-init", "html", context).length) {
         new BootstrapEnhancedDropdowns({
-          selector: 'ul.menu.enhanced-dropdowns',
+          selector: "ul.menu.enhanced-dropdowns",
           autoInit: true,
         });
       }
