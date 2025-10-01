f3f309f0e06cf907f05d75d4705eba154ad6b6a7
Drazen Musa <drazenmusa@yahoo.com>
2025-10-01 13:53:07 +0200

    fix: Remove bloated dxpr-theme-settings.min.js and add Babel runtime plugin Fixes #611

--- webpack.config.js
+++ webpack.config.js
@@ -15,11 +15,40 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        include: path.resolve(__dirname, 'js/dist/settings-admin'),
+        type: 'javascript/dynamic',
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'],
+            babelrc: false,
+            presets: [
+              [
+                '@babel/preset-env',
+                { targets: { ie: '11' }, modules: 'commonjs' }
+              ]
+            ],
+            plugins: [
+              ['@babel/plugin-transform-runtime', { useESModules: false }]
+            ],
+          },
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/, path.resolve(__dirname, 'js/dist/settings-admin')],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: false,
+            presets: [
+              [
+                '@babel/preset-env',
+                { targets: { ie: '11' }, modules: false }
+              ]
+            ],
+            plugins: [
+              ['@babel/plugin-transform-runtime', { useESModules: false }]
+            ],
           },
         },
       },
